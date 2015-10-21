8e7acb1806b63aaa661603fc65c229d6b7566055
GertieNyesh <gertienyesh@gmail.com>
2015-10-21 12:08:26 +0300

    Attempt to authenticate with Passport

--- server.js
+++ server.js
@@ -4,23 +4,60 @@ var app = express();
 var mongoose = require('mongoose');
 var parser = require('body-parser');
 var morgan = require('morgan');
-var Book = require('./models/books')
+var Book = require('./models/books');
+var path = require('path');
+var passport = require('passport');
+var passportLocal = require('passport-local');
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
+
 
 var port = process.env.PORT || 2020
 var db = 'mongodb://sheshe:sheshe@ds051873.mongolab.com:51873/books';
 
 //Database connect
 mongoose.connect(db);
-//Environment. Parser
+
+app.set('view engine', 'ejs');
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/views'));
+
+//Environment. Parser Middleware configuration
 app.use(parser.urlencoded({extended: true}));
 app.use(parser.json());
+app.use(cookieParser());
+app.use(expressSession({secret : process.env.SESSION_SECRET || 'secret',
+  resave : false,
+  saveUninitialized : false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use(morgan('dev'));
+passport.use(new passportLocal.Strategy(function(username, password, done) {
+  if (username === password) {
+    done (null, {id : username, name : username});
+  } else {
+    done (null, null);
+  }
+}));
+
+//Serialize
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  done(null, {id : id, name : id});
+})
+
 
 //Routes
 var route1 = express.Router();
 
 route1.route('/books').post(function (req, res) {
+  if ({isAuthenticated : false}) {
+    res.redirect('/');
+  } else
   var book = new Book();
   book.name = req.body.name;
   book.rating = req.body.rating;
@@ -28,7 +65,8 @@ route1.route('/books').post(function (req, res) {
   //Save the new data
   book.save(function (err) {
     if (err) throw err;
-      res.json({message: "Recorded!"});
+      // res.json({message: "Recorded!"});
+      res.send("Saved!")
   });
 });
 
@@ -67,8 +105,16 @@ route1.route('/books/:book_id').put(function (req, res) {
 
 
 route1.get('/', function (req, res) {
-  res.json({message: "wELCOME TO THE API"});
-});
+  // res.render('login', {
+  //   isAuthenticated: req.isAuthenticated(),
+  //   user: req.user
+  res.render('login');
+  });
+
+route1.post('/', passport.authenticate('local'), function (req, res) {
+  res.redirect('api/books');
+})
+
 
 app.use('/api', route1);
 
